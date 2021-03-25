73d88c552862b66ce1033ac88ead4ba6d4589a70
dmitiy <shkarlat.dmitry@yandex.ru>
2021-03-25 18:57:13 +0300

    add: modal and info

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React, { Component } from "react";
 import { getDataFromApi } from "./api/getDataFromApi";
 import Search from "./components/Search";
-import { Table, Button } from "antd";
+import { Table, Button, Modal, Tag } from "antd";
 
 const columns = [
+  {
+    title: "№",
+    dataIndex: "iterationId",
+    key: "iterationId",
+  },
   {
     title: "ID",
     dataIndex: "id",
@@ -27,6 +32,8 @@ class App extends Component {
     filterValue: "",
     page: 1,
     hasMore: false,
+    isDisplayModal: false,
+    dataModal: {},
   };
 
   componentDidMount() {
@@ -60,15 +67,32 @@ class App extends Component {
     );
   };
 
+  showModal = (data) => {
+    this.setState({
+      isDisplayModal: true,
+      dataModal: data,
+    });
+  };
+
+  hideModal = () => {
+    this.setState({
+      isDisplayModal: false,
+    });
+  };
+
   render() {
-    const { illustration, filterValue, hasMore } = this.state;
+    const { illustration, filterValue, hasMore, dataModal } = this.state;
 
     const tableData = illustration
-      .map(({ id, name, createdDate }, index) => ({
+      .map(({ id, name, createdDate, cover, keywords, pack }, index) => ({
+        iterationId: index + 1,
         key: `${id}_${index}`,
         id: `${id}`,
         name,
         createdDate,
+        cover,
+        keywords,
+        pack,
       }))
       .filter(
         ({ id, name, createdDate }) =>
@@ -81,12 +105,66 @@ class App extends Component {
     return (
       <div className='app'>
         <Search data={illustration} onChange={this.onSearchChange}></Search>
-        <Table dataSource={tableData} columns={columns} pagination={false} />
+        <Table
+          dataSource={tableData}
+          columns={columns}
+          pagination={false}
+          onRow={(record, rowIndex) => {
+            return {
+              onClick: (event) => {
+                console.log(record);
+                this.showModal(record);
+              },
+            };
+          }}
+        />
         {hasMore && (
-          <Button type='primary' onClick={this.addNextPageData}>
+          <Button
+            type='primary'
+            className='btn-addMore'
+            onClick={this.addNextPageData}
+            block
+          >
             Показать еще
           </Button>
         )}
+        <Modal
+          title='Подробная информация'
+          okText='Ok'
+          visible={this.state.isDisplayModal}
+          onCancel={this.hideModal}
+          onOk={this.hideModal}
+        >
+          <ul>
+            <li>
+              <p>
+                Раздел:
+                {dataModal.pack && (
+                  <Tag color='blue'>{dataModal.pack.name}</Tag>
+                )}
+              </p>
+            </li>
+            <li>
+              <p>ID: {dataModal.id} </p>
+              <p>Название: {dataModal.name}</p>
+              <p>Дата создания: {dataModal.createdDate}</p>
+            </li>
+            <li>
+              <p>
+                Ключевые слова:
+                {dataModal.keywords &&
+                  dataModal.keywords.map((tag, idx) => (
+                    <Tag color='green' key={tag + Date.now()}>
+                      {tag}
+                    </Tag>
+                  ))}
+              </p>
+            </li>
+            <li>
+              <img src={dataModal.cover} width={272} alt={dataModal.name} />
+            </li>
+          </ul>
+        </Modal>
       </div>
     );
   }
--- src/components/Search.js
+++ src/components/Search.js
@@ -2,20 +2,22 @@ import React, { Component } from "react";
 import { Input, Row, Col, Form } from "antd";
 
 class Search extends Component {
-  state = {};
   onChangeHandler = (e) => {
     const str = e.target.value.trim();
     this.props.onChange(str);
   };
+
   render() {
     return (
       <>
         <Form>
           <Row gutter={[16, 16]}>
-            <Col span={10}>
+            <Col span={24}>
               <Input
                 placeholder='Введите слово для поиска ...'
                 onChange={this.onChangeHandler}
+                size='large'
+                style={{ marginBottom: 24 }}
               />
             </Col>
           </Row>
